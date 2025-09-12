47c587a37849cf403ab64ac320b4f3ea81f2cc78
Nas <143642674+AriaNas202@users.noreply.github.com>
2025-09-12 13:44:38 -0400

    Init floating_point_library.s (Same as Keypad project)

---
+++ floating/floating_point_library.s
@@ -0,0 +1,262 @@
+
+
+
+
+
+ .text
+ 	.global gpio_init
+	.global uart_init
+    .global uart_interrupt_init
+	.global output_string
+	.global output_character
+    .global simple_read_character
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+gpio_init:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+;INIT for keypad
+
+    ;(1) SETUP Enable Clock (Port A, Pins 5-2) (Port D, Pins 3-0)
+    MOV r1, #0xE608
+    MOVT r1, #0x4000
+    ADD r1, #0xF0000      ;put clock register in r1
+    LDR r2, [r1]        ;loads current clock info into r2
+    ORR r2, r2, #0x1       ;Set 0th bit to 1 (Port A)
+    ORR r2, r2, #0x8       ;Set 3ed bit to 1 (Port D)
+    STR r2, [r1]        ;store new clock with Port A/D enabled
+
+    ;CHECK to see if clock is enabled (PRGPIO)
+    ;Check A Port
+CheckAClock:
+	MOV r0, #0xE000
+	MOVT r0, #0x400F
+	LDR r1, [r0, #0xA08]	;Get PRGPIO Reg data in r1
+	AND r1, r1, #1			;mask 0th bit for Port A
+	CMP r1, #0				;if r1==0, then Port is NOT ready
+	BEQ CheckAClock			;if r1==0, then we keep looping
+
+;Check D Port
+CheckDClock:
+	MOV r0, #0xE000
+	MOVT r0, #0x400F
+	LDR r1, [r0, #0xA08]	;Get PRGPIO Reg data in r1
+	AND r1, r1, #0x8		;mask 3ed bit for Port D
+	CMP r1, #0				;if r1==0, then Port is NOT ready
+	BEQ CheckDClock			;if r1==0, then we keep looping
+
+
+
+    ;Port A, Pin 5,4,3,2
+    ;Enable Direction for Pins (offset 0x400)
+    MOV r1, #0x4000
+    MOVT r1, #0x4000        ;Move base address for Port A in r1
+    LDR r2, [r1, #0x400]    ;load pin direction register into r2
+    ORR r2, r2, #0x3C      	;sets Pin 2-5 bit to Output (WRITING TO THOSE PINS)
+    STR r2, [r1, #0x400]    ;stores the masked value back in directional register
+
+    ;Set as Digital
+    LDR r2, [r1, #0x51C]    ;Loads Digital Mode Register into r2
+    ORR r2, r2, #0x3C        ;sets Pin 2,3,4,5 Bit with Mask to 1 (Enables Digital Mode)
+    STR r2, [r1, #0x51C]    ;stores masked register back
+
+    ;Port D, Pin 0,1,2,3
+    ;Enable Direction for Pins (offset 0x400)
+    MOV r1, #0x7000
+    MOVT r1, #0x4000        ;Move base address for Port D in r1
+    LDR r2, [r1, #0x400]    ;load pin direction register into r2
+    BIC r2, r2, #0xF        ;sets Pin 0,1,2,3 bit to input (READING THOSE PINS)
+    STR r2, [r1, #0x400]    ;stores the masked value back in directional register
+
+    ;Set as Digital
+    LDR r2, [r1, #0x51C]    ;Loads Digital Mode Register into r2
+    ORR r2, r2, #0xF        ;sets Pin 0,1,2,3 Bit with Mask to 1 (Enables Digital Mode)
+    STR r2, [r1, #0x51C]    ;stores masked register back
+
+
+
+    ;do we need Pull Up Register? I really dont think So (it was for Tiva Buttons, not Alice) (Anthony said it doesnt! YAY!)
+
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+	MOV pc, lr
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+uart_init:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+
+    ; Provide clock to UART0
+    MOV r4, #0xE618
+    MOVT r4, #0x400F
+    MOV r1, #1
+    STR r1, [r4]
+
+    ; Enable clock to PortA
+    MOV r4, #0xE608
+    MOVT r4, #0x400F
+    MOV r1, #1
+    STR r1, [r4] ;(THIS PART BREAKS THE CODE, HUH? This disables Port D, Omg, I cannot believe I found it, im so tired)
+
+    ; Disable UART0 Control
+    MOV r4, #0xC030
+    MOVT r4, #0x4000
+    MOV r1, #0
+    STR r1, [r4]
+
+    ; Set UART0_IBRD_R for 115,200 baud
+    MOV r4, #0xC024
+    MOVT r4, #0x4000
+    MOV r1, #8
+    STR r1, [r4]
+
+    ; Set UART0_FBRD_R for 115,200 baud
+    MOV r4, #0xC028
+    MOVT r4, #0x4000
+    MOV r1, #44
+    STR r1, [r4]
+
+    ; Use System Clock
+    MOV r4, #0xCFC8
+    MOVT r4, #0x4000
+    MOV r1, #0
+    STR r1, [r4]
+
+    ; Use 8-bit word length, 1 stop bit, no parity
+    MOV r4, #0xC02C
+    MOVT r4, #0x4000
+    MOV r1, #0x60
+    STR r1, [r4]
+
+    ; Enable UART0 Control
+    MOV r4, #0xC030
+    MOVT r4, #0x4000
+    MOV r1, #0x301
+    STR r1, [r4]
+
+    ; Make PA0 and PA1 as Digital Ports
+    MOV r4, #0x451C
+    MOVT r4, #0x4000  ;r4 - address
+    LDR r0, [r4] ;r0- address value
+    ;LDR r1, [r0]
+    ORR r1, r0, #0x03 ;masked value
+    STR r1, [r4]
+
+
+
+    ; Change PA0,PA1 to Use an Alternate Function
+    MOV r4, #0x4420
+    MOVT r4, #0x4000
+    LDR r0, [r4] ;r0- address value
+    ;LDR r1, [r0]
+    ORR r1, r0, #0x03 ;masked value
+    STR r1, [r4]
+
+    ; Configure PA0 and PA1 for UART
+    MOV r4, #0x452C
+    MOVT r4, #0x4000
+    LDR r0, [r4] ;r0- address value
+    ;LDR r1, [r0]
+    ORR r1, r0, #0x11 ;masked value
+    STR r1, [r4]
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+	MOV pc, lr
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+uart_interrupt_init:
+	PUSH {r4-r12,lr}
+
+	MOV r0, #0xC000
+	MOVT r0, #0x4000
+	LDR r1, [r0, #0x038]
+
+	ORR r1,r1, #0x10	;set 5th bit
+
+	STR r1, [r0, #0x038]
+
+
+	;Config Processor to Allow UART Interrupts
+	MOV r0, #0xE000
+	MOVT r0, #0xE000
+	LDR r1, [r0, #0x100]
+
+	ORR r1,r1,#0x20
+
+	STR r1, [r0, #0x100]
+
+	POP {r4-r12,lr}
+	MOV pc, lr
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+output_string:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+;ARGUMENTS
+    ;r0    - Original Base of string address
+    ;      - Will turn into "current char" argument
+    ;r4    - New base of String Address
+
+    MOV r4, r0              ;copy base address into r4
+
+GetChar:
+    LDRB r0, [r4]           ;load current char into r0
+
+    CMP r0, #0              ;compare current char to NULL (Is this the End of the String?)
+    BEQ EndOutputString     ;If current char is NULL, were done printing the string, branch to end
+
+    BL output_character     ;call function to print char in r0 as argument
+
+    ADD r4,r4,#1            ;incrament base address to the next char
+
+    B GetChar               ;Branch to handle the next char
+
+
+EndOutputString:
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+	MOV pc, lr
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+output_character:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+
+        MOV r1, #0xC018
+        MOVT r1, #0x4000    ;load flag reg address in r1
+        MOV r3, #0x20       ;load mask 5th bit in r3
+
+Polling:
+        LDRB r2, [r1]       ;load flag into r2
+        AND r2,r2,r3        ;mask r2 to the 5th bit
+
+        CMP r2, #0          ;does the mask flag == 0?
+        BNE Polling         ;If r2!=0 (r2==1) then we keep polling until we get 1
+
+        MOV r1, #0xC000
+        MOVT r1, #0x4000    ;Load data address into r1
+
+        STRB r0, [r1]       ;store argument in r0 into data register at r1
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+	MOV pc, lr
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+simple_read_character:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+    MOV r4, #0xC000     ;UART base address
+    MOVT r4, #0x4000
+    LDRB r5, [r4, #0x18] ;Load from memory
+    LDRB r0, [r4]       ;Store in r0
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+
+	MOV pc, lr	; Return
+
+
+.end
