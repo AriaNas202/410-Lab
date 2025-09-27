af28278ead9d5652cb412b4b707c30297b59cd59
Nas <143642674+AriaNas202@users.noreply.github.com>
2025-09-27 17:58:00 -0400

    Blinky Working, Added Menu, None Advanced Started

--- pwm_timer/pwm_with_general_purpose_timer.s
+++ pwm_timer/pwm_with_general_purpose_timer.s
@@ -4,6 +4,7 @@
 	.global blinkyMenu
 	.global advancedMenu
 	.global lightState
+	.global blinkAlternateFlag
 
 
 mainMenu:        	.string "What would you like to test?", 0xA, 0xD
@@ -14,6 +15,7 @@ blinkyMenu:			.string "You are currently testing Blinky", 0xA, 0xD
 					.string "Press any Key to Go Back to Main Menu", 0
 advancedMenu:		.string "This is a temp advanced menu, eventually we will need to get a user inputted color", 0
 lightState:			.word 0x0
+blinkAlternateFlag:	.word 0x0
 
 
 
@@ -27,6 +29,8 @@ lightState:			.word 0x0
 	.global pwmTimer
 	.global Timer_Handler
 	.global UART0_Handler
+	.global Advanced_RGB_LED
+	.global blinky
 	.global timer_interrupt_init ;Library
 	.global gpio_init ;Library
 	.global uart_init ;Library
@@ -35,7 +39,8 @@ lightState:			.word 0x0
 	.global output_character ;Library
 	.global simple_read_character ;Library
     .global modified_int2string ;Library
-    .global string2int ;Library
+    .global modified_illuminate_RGB_LED ;Library
+    ;.global string2int ;Library
 
 
 
@@ -43,21 +48,12 @@ ptr_mainMenu:				.word mainMenu
 ptr_blinkyMenu:				.word blinkyMenu
 ptr_advancedMenu:			.word advancedMenu
 ptr_lightState:				.word lightState
+ptr_blinkAlternateFlag:		.word blinkAlternateFlag
 
 
 
 
-blinky:
-	PUSH  {r4-r12, lr}	; Store register lr on stack
-	;change timer interrupt speed (not right now)
 
-	;change state flag to 1 (we're testing Blinky)
-	ldr r0, ptr_lightState
-	MOV r1, #1
-	STR r1, [r0]
-
-	POP  {r4-r12, lr}	; Store register lr on stack
-	MOV pc, lr
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;This is the second function to be called by MAIN
 ;actually does the little game or whatever
@@ -69,7 +65,12 @@ pwmTimer:
 	BL uart_init				;Print Menu and Test Functions
 	bl uart_interrupt_init		;config to uart interrupt to get inputs
 	BL gpio_init				;configs GPIO so we can use light
-	bl timer_interrupt_init		;configures timer to interrupt
+	;bl timer_interrupt_init		;configures timer to interrupt
+
+resetPwmTimer:
+	;Clear screen
+	MOV r0, #0xC
+	BL output_character
 
 	;Print Main Menu to Screen
 	LDR r0, ptr_mainMenu
@@ -91,19 +92,27 @@ mainMenuPoll:
 	B mainMenuPoll
 
 blinkyMain:
+	;clear screen
+	MOV r0, #0xC
+	BL output_character
+
 	;print blinky menu
 	ldr r0, ptr_blinkyMenu
 	bl output_string
 
 blinkyPoll:
-	B blinkyPoll	;normally we would try to exit this at a certain point but im putting infin loop here to test it
+	;The Light is blinking, State Currently #1
+	;Wait for state #0 to do anything
+	ldr r4, ptr_lightState
+	ldr r4, [r4]
+	CMP r4, #0
+	BEQ resetPwmTimer
+	B blinkyPoll
 
 
 advancedMain:
 advancedPoll:
 quitMain:
-
-
 
 	POP {r4-r12, lr}
 	MOV pc, lr
@@ -124,13 +133,49 @@ Timer_Handler:
 	ORR r1, r1, #0x1
 	STRB r1, [r0, #0x24]
 
-	;just using this to test uart handler
+	;read light state flag (r0)
 	LDR r0, ptr_lightState
-	ldr r0, [r0]
-	ADD r0, r0, #0x30
-	BL output_character
+	LDR r0, [r0]
+
+	;See which function we're testing based on LightState Flag
+	CMP r0, #0
+	BEQ noneTimerHandler		;if 0, turn off all lights
+	CMP r0, #1
+	BEQ blinkyTimerHandler		;if 1 blink lights
+	CMP r0, #2
+	BEQ advancedTimerHandler	;if 2 do advanced processing
+	B endTimerHandler
 
 
+noneTimerHandler:
+	;Testing No Function- turn all lights off
+	MOV r0, #0
+	BL modified_illuminate_RGB_LED
+	B endTimerHandler
+
+blinkyTimerHandler: ;Testing Blinky
+	;Get Alternate Flag (r1-addres, r0-data)
+	LDR r1, ptr_blinkAlternateFlag
+	LDR r0, [r1]
+
+	;Change Alt Flag to opposite
+	EOR r0, r0, #1
+
+	;Store New Flag
+	STR r0, [r1]
+
+	;Change the light to the new flag state
+	BL modified_illuminate_RGB_LED
+
+	;end
+	B endTimerHandler
+
+
+
+
+
+advancedTimerHandler:
+endTimerHandler:
 
 	POP {r4-r12,lr}
 	BX lr       	; Return
@@ -158,39 +203,100 @@ UART0_Handler:
 
     ;do something based on what the state is
     CMP r2, #0
-    BEQ uart0State
+    BEQ uart0State	;We're On Main Menu
     CMP r2, #1
-    BEQ uart1State
+    BEQ uart1State	;We're Testing Blinky
     CMP r2, #2
-    BEQ uart2State
+    BEQ uart2State	;We're Testing Advanced
     B EndUartHandler
-
+;;;;;;;;;;;;;;;
 uart0State:
 	;check input to see what we do
 	CMP r0, #0x31 ;user hit 1
 	BEQ state01
+	CMP r0, #0x32 ;user hit 2
+	BEQ state02
 	B state0else
 
-state01:
+state01:	;test Blinky
 	BL blinky
 	B EndUartHandler
+state02:	;test Advanced
+	BL Advanced_RGB_LED
+	B EndUartHandler
+state0else:	;Quit
+	;change flag to #3
+	MOV r2, #3
+	str r2, [r1]
+	B EndUartHandler
 
-state02:
-state0else:
 
+
+
+;;;;;;;;;;;;;;;
 uart1State:
+	;Change flag to #0
+	MOV r2, #0
+	str r2, [r1]
+	B EndUartHandler
+
+
+;;;;;;;;;;;;;;;
 uart2State:
-
-
-
-
-
 EndUartHandler:
-
-
 	;End
 	POP {r4-r12,lr} ; Pop registers from stack
 	BX lr       	; Return
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Advanced_RGB_LED:
+	PUSH  {r4-r12, lr}	; Store register lr on stack
+	;change timer interrupt speed to interrupt 100 times a second
+	MOV r0, #1
+	bl timer_interrupt_init
+
+	;change state flag to 1 (we're testing Blinky)
+	ldr r0, ptr_lightState
+	MOV r1, #2
+	STR r1, [r0]
+
+	POP  {r4-r12, lr}	; Store register lr on stack
+	MOV pc, lr
+
+
+blinky:
+	PUSH  {r4-r12, lr}	; Store register lr on stack
+	;change timer interrupt speed to interrupt 2 times a second
+	MOV r0, #0
+	bl timer_interrupt_init
+
+	;change state flag to 1 (we're testing Blinky)
+	ldr r0, ptr_lightState
+	MOV r1, #1
+	STR r1, [r0]
+
+	POP  {r4-r12, lr}	; Store register lr on stack
+	MOV pc, lr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
