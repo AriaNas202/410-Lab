6b30039fb40a9da3438b3b93861f01f45d3a8e27
Nas <143642674+AriaNas202@users.noreply.github.com>
2025-10-03 16:51:38 -0400

    NOTHING WORKING (blinky attempt made)

--- pwm_generator/pwm_with_pwm_generator.s
+++ pwm_generator/pwm_with_pwm_generator.s
@@ -111,50 +111,50 @@ init_pwm_generator:
 blinky:
 	PUSH {r4-r12, lr}
 
-	;GOING TO ENABLE THIS ON PWM MODULE 0, GENERATOR 0, PIN 0(A)
-	;M0PWM0 (Module 0)-> PB6 (port B, Pin 6)
+	;GOING TO ENABLE THIS ON:
+	;M1PWM5 (Module 1, OUTPUT 5)-> PF1 (port F, Pin 1)
 
-	;Enable PWM Clock
-	;RCG0 (CURRENTLY USING LEGACY REGISTER--Actually notes say to use this, so okay)
+	;!Enable PWM Clock
+	;RCGCPWM (QUESTION: the instructions use a legacy register, HOW did he find THIS register???)
+	;Module PWM 1
 	;(r0-address; r1-data)
-	;SEEMS TO WORK
 	MOV r0, #0xE000
 	MOVT r0, #0x400F
-	ADD r0, r0, #0x100	;get effective address
-	MOV r1, #0x100000	;Set 20th bit on
+	ADD r0, r0, #0x640	;get effective address
+	MOV r1, #0x2		;Set 2nd Bit for PWM Module #1
 	STR r1, [r0]		;Update Register Data
 
-	;Enable GPIO Clock (Im gonna use non legacy for this one)
-	;Port B
+	;XEnable GPIO Clock (Im gonna use non legacy for this one)
+	;Port F
 	;(r0-address; r1-data)
 	;SEEMS TO WORK
 	MOV r0, #0xE000
 	MOVT r0, #0x400F
 	ADD r0, r0, #0x608	;get effective address
-	MOV r1, #0x2		;set second bit (Port B)
+	MOV r1, #0x20		;set fifth bit (Port F)
 	STR r1, [r0]		;Update Register Data
 
-	;Enable Pins for Alt Functio n
-	;PORT B (APB, first one)
-	;Pin 6
+	;XEnable Pins for Alt Function
+	;PORT F (APBee, first one)
+	;Pin 1
 	;(r0-address, r1-data)
-	;(i cannot find where this is updating)
+	;UPDATES GPIO_AFSEL successfully
 	MOV r0, #0x5000
-	MOVT r0, #0x4000
+	MOVT r0, #0x4002
 	ADD r0, r0, #0x420	;get effective address
-	MOV r1, #0x40		;set 6th pin
+	MOV r1, #0x2		;set 1st pin
 	STR r1, [r0]		;Update Register Data
 
-	;Tell the Pins WHICH Alt Function
-	;Need to write #4 in field
-	;PMC0-7 is the pin (we want Pin 6)
-	;Port B
+	;XTell the Pins WHICH Alt Function
+	;Need to write #5 in field
+	;PMC0-7 is the pin (we want Pin 1)
+	;Port F
 	;(r0-address; r1-data)
-	;This seems to update GPIO_PCTL successfully
+		;(I cannot see where this is updating)
 	MOV r0, #0x5000
-	MOVT r0, #0x4000
+	MOVT r0, #0x4002
 	ADD r0, r0, #0x52C		;Get effective address
-	MOV r1, #0x4000000		;Set Pin 6 area as #4
+	MOV r1, #0x50			;Set Pin 1 area as #5
 	STR r1, [r0]			;Update Register Data
 
 	;SKIP setting RCC Data because I legit Do not Care about modifying the 16MHz clock
@@ -163,39 +163,68 @@ blinky:
 
 	;;;;;EVERYTHING UP TO HERE DOESNT BREAK THE TIVA (which means if i do break it then the error is after this);;;;;;;;;;;;;;;;
 
-	;Configure the PWM Generator for coun DOWN  Mode/ Immediate Updates
+	;XConfigure the PWM Generator for coun DOWN  Mode/ Immediate Updates
 	;The Default 0x0 setting is DOWN/Immediate Updates, so Im just writing 0x0 to this to be safe
+	;he does NOT do anything about this in the slides, probably because it's all default, but Im keeping
 	;(r0-address; r1-data)
-	;(i cannot find where this is updating)
-	MOV r0, #0x8000
+	MOV r0, #0x9000
 	MOVT r0, #0x4002
-	ADD r0, r0, #0x40	;Get effective address
+	ADD r0, r0, #0x0C0	;Get effective address (we're on output 5, so using generator 2)
 	MOV r1, #0x0		;Set to clear to default settings
 	STR r1, [r0]		;Update Register Data
 
-	;Configure Output A
+	;!Configure Output A
 	;(r0-address; r1-data)
-	;This seems to be updating PWM_0_GENA successfully
-	MOV r0, #0x8000
+	MOV r0, #0x9000
 	MOVT r0, #0x4002
-	ADD r0, r0, #0x60	;Get effective address
+	ADD r0, r0, #0x0E4	;Get effective address (we're on output 5, so using generator 2)
 		;00  B Down (NO B)
 		;00  B Up (NO UP)
 		;10 A Down --> DRIVE A LOW
 		;00 A Up (NO UP)
 		;11 When Counter == PWMnLOAD --> DRIVE A HIGH
 		;00 When counter == 0 --> NOTHING
-	MOV r1, #0x008C		;Im pretty sure the variable we're doing is literally the exact same as the docs
+	MOV r1, #0x008C		;USING SAME AS DOCS (idk if its right)
 	STR r1, [r0]		;Update Register Data
 
 
 	;Do NOT need to configure output B (there is only 1 output and its an A pin)
 
-	;SET THE PERIOD! (Frequency) (How often it's "interrupted")
+	;!SET THE PERIOD (Frequency) (How often it's "interrupted")
 	;For the Advanced Value, we dont particularly care how fast it is (I THINKKK), but blinky is every half second, so Period of 8 million (16MHz Clock)
 	;(r0-address; r1-data)
 
-	MOV r0, #0x
+	MOV r0, #0x9000
+	MOVT r0, #0x4002
+	ADD r0, r0, #0x0D0 		;Get effective address (we're on output 5, so using generator 2)
+	;set the value to the required period minus 1
+	;OK the problem here is that the load value is NOT big enough to store 8 million, so temporarily Im gonna make it as big as i can to see if i can just get it working
+	MOV r1, #0xFFFF	;temp value (looks like i DO need to set rcc After all!!!)
+	STR r1, [r0]	;Update Register Data
+
+	;set the Pulse (duty Cycle)
+	MOV r0, #0x9000
+	MOVT r0, #0x4002
+	ADD r0, r0, #0x0D8
+	MOV r1, #0x7FFF	;this is totally wrong, but its 50% than the messed up value above
+	STR r1, [r0]
+
+	;Enable outputs
+	MOV r0, #0x9000
+	MOVT r0, #0x4002
+	ADD r0, r0, #0x0C0
+	MOV r1, #0x1		;set to enable the output
+	STR r1, [r0]
+
+	;The NOTES dont say to do this, but the DOCs do so im doing it
+	;Idk if it matters or not (can delete later) (Im so tired, PLEASE WORK)
+	;MOV r0, #0x9000
+	;MOVT r0, #0x4002
+	;ADD r0, r0, #0x8	;get address
+	;MOV r1, #0x20		;turn on 5th output
+	;STR r1, [r0]
+
+
 
 
 
