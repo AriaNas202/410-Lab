ba5beffe775cb6d511edb29662e96c0b2ec1583c
Nas <143642674+AriaNas202@users.noreply.github.com>
2025-10-03 13:43:37 -0400

    Started Blinky (WRONG GENERATOR/PIN USED!!!)

--- pwm_generator/pwm_with_pwm_generator.s
+++ pwm_generator/pwm_with_pwm_generator.s
@@ -74,11 +74,189 @@ pwmGen:
 	;BL gpio_init				;configs GPIO so we can use light
 	;bl timer_interrupt_init		;configures timer to interrupt
 
+	BL blinky
+
+infinLoop:
+	B infinLoop
+
 
 quitMain:
 
 	POP {r4-r12, lr}
 	MOV pc, lr
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+init_pwm_generator:
+	PUSH {r4-r12, lr}
+
+
+
+
+
+
+
+
+	POP {r4-r12, lr}
+	MOV pc, lr
+
+
+
+
+
+;I JUST HAD THE REALIZATION THAT IVE BEEN WORKING ON THE WRONG PIN THIS ENTIRE TIME
+;THE SETUP IS THERE BUT I MUST FIX EVERYTHINGGGGGGG!!!!!
+blinky:
+	PUSH {r4-r12, lr}
+
+	;GOING TO ENABLE THIS ON PWM MODULE 0, GENERATOR 0, PIN 0(A)
+	;M0PWM0 (Module 0)-> PB6 (port B, Pin 6)
+
+	;Enable PWM Clock
+	;RCG0 (CURRENTLY USING LEGACY REGISTER--Actually notes say to use this, so okay)
+	;(r0-address; r1-data)
+	;SEEMS TO WORK
+	MOV r0, #0xE000
+	MOVT r0, #0x400F
+	ADD r0, r0, #0x100	;get effective address
+	MOV r1, #0x100000	;Set 20th bit on
+	STR r1, [r0]		;Update Register Data
+
+	;Enable GPIO Clock (Im gonna use non legacy for this one)
+	;Port B
+	;(r0-address; r1-data)
+	;SEEMS TO WORK
+	MOV r0, #0xE000
+	MOVT r0, #0x400F
+	ADD r0, r0, #0x608	;get effective address
+	MOV r1, #0x2		;set second bit (Port B)
+	STR r1, [r0]		;Update Register Data
+
+	;Enable Pins for Alt Functio n
+	;PORT B (APB, first one)
+	;Pin 6
+	;(r0-address, r1-data)
+	;(i cannot find where this is updating)
+	MOV r0, #0x5000
+	MOVT r0, #0x4000
+	ADD r0, r0, #0x420	;get effective address
+	MOV r1, #0x40		;set 6th pin
+	STR r1, [r0]		;Update Register Data
+
+	;Tell the Pins WHICH Alt Function
+	;Need to write #4 in field
+	;PMC0-7 is the pin (we want Pin 6)
+	;Port B
+	;(r0-address; r1-data)
+	;This seems to update GPIO_PCTL successfully
+	MOV r0, #0x5000
+	MOVT r0, #0x4000
+	ADD r0, r0, #0x52C		;Get effective address
+	MOV r1, #0x4000000		;Set Pin 6 area as #4
+	STR r1, [r0]			;Update Register Data
+
+	;SKIP setting RCC Data because I legit Do not Care about modifying the 16MHz clock
+	;controls how fast it counts down
+	;he says we need to use this but Do we really? IDk?
+
+	;;;;;EVERYTHING UP TO HERE DOESNT BREAK THE TIVA (which means if i do break it then the error is after this);;;;;;;;;;;;;;;;
+
+	;Configure the PWM Generator for coun DOWN  Mode/ Immediate Updates
+	;The Default 0x0 setting is DOWN/Immediate Updates, so Im just writing 0x0 to this to be safe
+	;(r0-address; r1-data)
+	;(i cannot find where this is updating)
+	MOV r0, #0x8000
+	MOVT r0, #0x4002
+	ADD r0, r0, #0x40	;Get effective address
+	MOV r1, #0x0		;Set to clear to default settings
+	STR r1, [r0]		;Update Register Data
+
+	;Configure Output A
+	;(r0-address; r1-data)
+	;This seems to be updating PWM_0_GENA successfully
+	MOV r0, #0x8000
+	MOVT r0, #0x4002
+	ADD r0, r0, #0x60	;Get effective address
+		;00  B Down (NO B)
+		;00  B Up (NO UP)
+		;10 A Down --> DRIVE A LOW
+		;00 A Up (NO UP)
+		;11 When Counter == PWMnLOAD --> DRIVE A HIGH
+		;00 When counter == 0 --> NOTHING
+	MOV r1, #0x008C		;Im pretty sure the variable we're doing is literally the exact same as the docs
+	STR r1, [r0]		;Update Register Data
+
+
+	;Do NOT need to configure output B (there is only 1 output and its an A pin)
+
+	;SET THE PERIOD! (Frequency) (How often it's "interrupted")
+	;For the Advanced Value, we dont particularly care how fast it is (I THINKKK), but blinky is every half second, so Period of 8 million (16MHz Clock)
+	;(r0-address; r1-data)
+
+	MOV r0, #0x
+
+
+
+
+
+
+
+	POP {r4-r12, lr}
+	MOV pc, lr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
